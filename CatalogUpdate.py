0568fef1be95998f68e57ee17310a017c7642def
m <tmptr101@gmail.com>
2025-10-14 23:04:43 +0300

    test to_excel
    Signed-off-by: m <tmptr101@gmail.com>

--- CatalogUpdate.py
+++ CatalogUpdate.py
@@ -1064,7 +1064,7 @@ class CreateTotalCsv(QThread):
 
                 loaded = 0
                 for i in range(1, report_parts_count + 1):
-                    df = pd.DataFrame(columns=["Ключ1 поставщика", "Артикул поставщика", "Производитель поставщика",
+                    header = ["Ключ1 поставщика", "Артикул поставщика", "Производитель поставщика",
                                                "Наименование поставщика",
                                                "Количество поставщика", "Цена поставщика", "Кратность поставщика",
                                                "Примечание поставщика", "01Артикул", "02Производитель",
@@ -1078,11 +1078,28 @@ class CreateTotalCsv(QThread):
                                                "Шаг опт", "Разрешения ПП", "УбратьЗП", "Предложений опт",
                                                "ЦенаБ", "Кол-во", "Код ПБ_П", "06Кратность", "Кратность меньше", "05Цена+",
                                                "Количество закупок", "% Отгрузки",
-                                               "Мин. Цена", "Мин. Поставщик"])
-                    df.to_csv(
-                        fr"{settings_data['catalogs_dir']}/pre Итог/pre Итог - страница {i}.csv",
-                        sep=';', decimal=',',
-                        encoding="windows-1251", index=False, errors='ignore')
+                                               "Мин. Цена", "Мин. Поставщик"]
+                    df = pd.DataFrame(columns=header)
+                                      # ["Ключ1 поставщика", "Артикул поставщика", "Производитель поставщика",
+                                      #          "Наименование поставщика",
+                                      #          "Количество поставщика", "Цена поставщика", "Кратность поставщика",
+                                      #          "Примечание поставщика", "01Артикул", "02Производитель",
+                                      #          "03Наименование", "05Цена", "06Кратность-", "07Код поставщика",
+                                      #          "09Код + Поставщик + Товар", "10Оригинал",
+                                      #          "13Градация", "14Производитель заполнен", "15КодТутОптТорг",
+                                      #          "17КодУникальности", "18КороткоеНаименование",
+                                      #          "19МинЦенаПоПрайсу", "20ИсключитьИзПрайса", "Отсрочка", "Продаём для ОС",
+                                      #          "Наценка для ОС", "Наценка Р", "Наценка ПБ", "Мин наценка", "Мин опт наценка",
+                                      #          "Наценка на оптовые товары", "Шаг градаци",
+                                      #          "Шаг опт", "Разрешения ПП", "УбратьЗП", "Предложений опт",
+                                      #          "ЦенаБ", "Кол-во", "Код ПБ_П", "06Кратность", "Кратность меньше", "05Цена+",
+                                      #          "Количество закупок", "% Отгрузки",
+                                      #          "Мин. Цена", "Мин. Поставщик"])
+                    # df.to_csv(
+                    #     fr"{settings_data['catalogs_dir']}/pre Итог/pre Итог - страница {i}.csv",
+                    #     sep=';', decimal=',',
+                    #     encoding="windows-1251", index=False, errors='ignore')
+                    df.to_excel(fr"{settings_data['catalogs_dir']}/pre Итог/pre Итог - страница {i}.xlsx", index=False, header=header)
                     req = select(TotalPrice_2.key1_s, TotalPrice_2.article_s, TotalPrice_2.brand_s, TotalPrice_2.name_s,
                                  TotalPrice_2.count_s, TotalPrice_2.price_s, TotalPrice_2.mult_s, TotalPrice_2.notice_s,
                                  TotalPrice_2._01article, TotalPrice_2._02brand, TotalPrice_2._03name,
@@ -1101,10 +1118,11 @@ class CreateTotalCsv(QThread):
                     # TotalPrice_2.reserve_count,
                     df = pd.read_sql_query(req, sess.connection(), index_col=None)
 
-                    df.to_csv(
-                        fr"{settings_data['catalogs_dir']}/pre Итог/pre Итог - страница {i}.csv",
-                        mode='a',
-                        sep=';', decimal=',', encoding="windows-1251", index=False, header=False, errors='ignore')
+                    # df.to_csv(
+                    #     fr"{settings_data['catalogs_dir']}/pre Итог/pre Итог - страница {i}.csv",
+                    #     mode='a',
+                    #     sep=';', decimal=',', encoding="windows-1251", index=False, header=False, errors='ignore')
+                    df.to_excel(fr"{settings_data['catalogs_dir']}/pre Итог/pre Итог - страница {i}.xlsx", index=False, header=header)
 
                     df_len = len(df)
                     loaded += df_len
@@ -1115,8 +1133,8 @@ class CreateTotalCsv(QThread):
 
                 for i in range(1, report_parts_count + 1):
                     shutil.copy(
-                        fr"{settings_data['catalogs_dir']}/pre Итог/pre Итог - страница {i}.csv",
-                        fr"{settings_data['catalogs_dir']}/Итог/Итог - страница {i}.csv")
+                        fr"{settings_data['catalogs_dir']}/pre Итог/pre Итог - страница {i}.xlsx",
+                        fr"{settings_data['catalogs_dir']}/Итог/Итог - страница {i}.xlsx")
 
             self.log.add(LOG_ID, f"Итог сформирован [{str(datetime.datetime.now() - cur_time)[:7]}]",
                          f"<span style='color:{colors.green_log_color};font-weight:bold;'>Итог</span> "
