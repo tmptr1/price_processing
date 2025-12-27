64ae72fb0ad89964e617a9196c24467fbd646345
m <tmptr101@gmail.com>
2025-12-27 22:39:27 +0300

    test del_dupl speed 3
    Signed-off-by: m <tmptr101@gmail.com>

--- Calculate.py
+++ Calculate.py
@@ -420,9 +420,11 @@ class CalculateClass(QThread):
 
         cur_time = datetime.datetime.now()
         # D3 не с макс. id среди D2
-        max_id_table = (select(self.TmpPrice_2._01article_comp, self.TmpPrice_2._14brand_filled_in, func.max(self.TmpPrice_2.id).label('max_id')).where(
-            self.TmpPrice_2._20exclude == 'D2').group_by(self.TmpPrice_2._01article_comp, self.TmpPrice_2._14brand_filled_in))
-        sess.execute(update(self.TmpPrice_2).where(self.TmpPrice_2.id==max_id_table.c.max_id).values(_20exclude='D3'))
+        # max_id_table = (select(self.TmpPrice_2._01article_comp, self.TmpPrice_2._14brand_filled_in, func.max(self.TmpPrice_2.id).label('max_id')).where(
+        #     self.TmpPrice_2._20exclude == 'D2').group_by(self.TmpPrice_2._01article_comp, self.TmpPrice_2._14brand_filled_in))
+        # sess.execute(update(self.TmpPrice_2).where(self.TmpPrice_2.id==max_id_table.c.max_id).values(_20exclude='D3'))
+        max_id_table = select(self.TmpPrice_2._01article_comp, self.TmpPrice_2._14brand_filled_in, func.max(self.TmpPrice_2.id).label('max_id')).where(self.TmpPrice_2._20exclude == 'D2').group_by(self.TmpPrice_2._01article_comp, self.TmpPrice_2._14brand_filled_in)
+        sess.execute(update(self.TmpPrice_2).where(self.TmpPrice_2.id == max_id_table.c.max_id).values(_20exclude='D3'))
         self.add_log(self.file_size_type, price_code, 'D3', cur_time)
 
         cur_time = datetime.datetime.now()
